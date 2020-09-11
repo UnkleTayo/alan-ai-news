65c88f627d6b4ef33ba14ec71afd5af4b06d62c7
Adetayo <akinsanyaadetayo7@gmail.com>
2020-09-11 04:35:21 +0100

    finished new card component

--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 import { Grid, Grow, Typoraphy } from '@material-ui/core';
+import {useStyles} from './style'
 
 const NewsCards = ({ articles }) => {
+  const classes = useStyles()
   return (
     <Grow in>
-      <Grid container alignItems='stretch'>
+      <Grid className={classes.container} container alignItems='stretch'>
         {articles.map((article, i) => (
           <Grid item xs={1} sm={6} lg={3} style={display: 'flex'}>
               <NewsCard article={article} i={i} />
           </Grid>
-        
-        ))}
+       
+        ))} 
        </Grow>
   );
 };
