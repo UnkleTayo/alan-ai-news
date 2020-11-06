9f947bc23c234b3b089f56c2536037eb94238be0
Adetayo <akinsanyaadetayo7@gmail.com>
2020-11-06 08:55:36 +0100

    👌 IMPROVE:  Minor fixes
    Signed-off-by: Adetayo <akinsanyaadetayo7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,6 +6,3 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// const alanKey =
-// '54eaf4f4e7efdb16678e65a153fa4d6a2e956eca572e1d8b807a3e2338fdd0dc/stage';
